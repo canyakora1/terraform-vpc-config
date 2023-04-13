84bcf4f1817efb1cad85083507ef2bc7028023d1
d-cyberguy <59446700+canyakora1@users.noreply.github.com>
2023-04-13 10:10:01 -0400

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "vpc_cidr" {
+  default = "10.10.0.0/16"
+}
+variable "public_subnets_cidr" {
+  type        = list(any)
+  default     = ["10.10.0.0/19", "10.10.32.0/19", "10.10.64.0/19"]
+  description = "Public subnet for Webservers servers"
+}
+variable "private_subnets_cidr" {
+  type        = list(any)
+  default     = ["10.10.128.0/19", "10.10.160.0/19", "10.10.192.0/19"]
+  description = "private subnet for Application servers"
+}
+variable "privatedb_subnet" {
+  type        = list(any)
+  default     = ["10.10.96.0/19", "10.10.224.0/19"]
+  description = "private subnet for Database servers"
+}
+#variable "nat_gw" {
+  #type        = string
+  #default     = public_subnets_cidr
+  #description = "description"
+#}
+
+variable "azs" {
+  type    = list(any)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
---
+++ vpc.tf
@@ -0,0 +1,61 @@
+# Create a custom VPC
+resource "aws_vpc" "cloudbootcamp_vpc" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    "Name" = "Cloud-Test1"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "cloud_test_igw" {
+  vpc_id = aws_vpc.cloudbootcamp_vpc.id
+  tags = {
+    "Name" = "main"
+  }
+}
+
+# Create Public Subnets
+resource "aws_subnet" "public_test1" {
+  count                   = length(var.public_subnets_cidr)
+  vpc_id                  = aws_vpc.cloudbootcamp_vpc.id
+  cidr_block              = element(var.public_subnets_cidr, count.index)
+  availability_zone       = element(var.azs, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "Subnet-${count.index + 1}"
+  }
+}
+
+# Create Private Subnets
+resource "aws_subnet" "private_test1" {
+  count                   = length(var.private_subnets_cidr)
+  vpc_id                  = aws_vpc.cloudbootcamp_vpc.id
+  cidr_block              = element(var.private_subnets_cidr, count.index)
+  availability_zone       = element(var.azs, count.index)
+  map_public_ip_on_launch = false
+  tags = {
+    "Name" = "Subnet-${count.index + 1}"
+  }
+}
+# Create a route-table
+resource "aws_route_table" "public_cloudboot_rt" {
+  vpc_id = aws_vpc.cloudbootcamp_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.cloud_test_igw.id
+  }
+  tags = {
+    "Name" = "PublicRouteTable"
+  }
+}
+# Create the Route table subnet association
+resource "aws_route_table_association" "a" {
+  count          = length(var.public_subnets_cidr)
+  subnet_id      = element(aws_subnet.public_test1.*.id, count.index)
+  route_table_id = aws_route_table.public_cloudboot_rt.id
+}
+# Create a Nat Gateway
+resource "aws_nat_gateway" "dcg_nat_gw" {
+  connectivity_type = "private"
+  subnet_id         = aws_subnet.private_test1[count.index]
+}
